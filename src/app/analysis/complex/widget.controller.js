362437102dd0aa129470f7d16dfe951ad783a358
chbohm <chbohm@yahoo.com.ar>
2015-05-05 13:49:12 -0300

    widget controller has widgetWith and widgetHeight properties

--- src/app/analysis/complex/widget.controller.js
+++ src/app/analysis/complex/widget.controller.js
@@ -1,24 +1,45 @@
 'use strict';
 angular.module('analysis').controller('WidgetController', ['$scope', '$modal',
-	function($scope, $modal) {
+  function($scope, $modal) {
 
-		$scope.remove = function(widget) {
-			$scope.model.widgets.splice($scope.model.widgets.indexOf(widget), 1);
-		};
+    $scope.widgetFullWidth = 0;
+		$scope.widgetFullHeight = 0;
+		$scope.widgetContentWidth = 0;
+		$scope.widgetContentHeight = 0;
 
-		$scope.openSettings = function(widget) {
-			console.log(widget);
-			$modal.open({
-				scope: $scope,
-				templateUrl: widget.settingsTemplate,
-				controller: 'WidgetSettingsCtrl',
-				resolve: {
-					widget: function() {
-						return widget;
-					}
-				}
-			});
-		};
+		$scope.$on('gridster-item-initialized', function(item) {
+			$scope.syncWidgetSize(item.targetScope.gridsterItem);
+		});
 
-	}
+    $scope.$on('gridster-item-resized', function(item) {
+			$scope.syncWidgetSize(item.targetScope.gridsterItem);
+    });
+
+
+    $scope.syncWidgetSize = function(gridsterItem) {
+			$scope.widgetFullWidth = gridsterItem.getElementSizeX();
+			$scope.widgetFullHeight = gridsterItem.getElementSizeY();
+			$scope.widgetContentWidth = $scope.widgetFullWidth - 27;
+			$scope.widgetContentHeight = $scope.widgetFullHeight - 50;
+    }
+
+    $scope.remove = function(widget) {
+      $scope.model.widgets.splice($scope.model.widgets.indexOf(widget), 1);
+    };
+
+    $scope.openSettings = function(widget) {
+      console.log($scope.gridsterItem);
+      $modal.open({
+        scope: $scope,
+        templateUrl: widget.settingsTemplate,
+        controller: 'WidgetSettingsCtrl',
+        resolve: {
+          widget: function() {
+            return widget;
+          }
+        }
+      });
+    };
+
+  }
 ])
